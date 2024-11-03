cc54303995631e68ddbfc4c77a822217d8253e56
kasia.kordys <kasia.kordys@gmail.com>
2024-11-03 17:09:47 +0200

    calculate and display the totals for accounts

--- src/components/Dashboard/AccountDetails.js
+++ src/components/Dashboard/AccountDetails.js
@@ -16,7 +16,39 @@ const AccountDetails = () => {
 
   // get account from store, destructed accounts
   const { account, loading, error } = useSelector((state) => state?.accounts);
-  console.log("transactions:", account?.data?.transactions);
+
+  const transactions = account?.data?.transactions;
+  console.log("transactions:", transactions);
+
+  // filter income
+  const income = transactions?.filter((t) => {
+    return t.transactionType === "Income";
+  });
+
+  // calc total income
+  const totalIncome = income?.reduce((acc, curr) => {
+    return acc + curr.amount;
+  }, 0);
+
+  console.log("totalIncome", totalIncome);
+
+  // filter expenses
+  const expenses = transactions?.filter((t) => {
+    return t.transactionType === "Expenses";
+  });
+
+  // calc total expenses
+  const totalExpenses = expenses?.reduce((acc, curr) => {
+    return acc + curr.amount;
+  }, 0);
+
+  console.log("totalExpenses", totalExpenses);
+
+  // calc total balance
+  const totalBalance = totalIncome - totalExpenses;
+
+  console.log("totalBalance", totalBalance);
+
   return (
     <>
       {loading && (
@@ -53,7 +85,7 @@ const AccountDetails = () => {
             <div className="flex flex-wrap justify-center -mx-4">
               <div className="w-full md:w-1/3 lg:w-1/4 px-4 mb-8 lg:mb-0">
                 <h2 className="mb-2 text-4xl md:text-5xl text-red-600 font-bold tracking-tighter">
-                  $900
+                  $ {totalExpenses}
                 </h2>
                 <p className="text-lg md:text-xl text-coolGray-500 font-medium">
                   Expenses
@@ -61,7 +93,7 @@ const AccountDetails = () => {
               </div>
               <div className="w-full md:w-1/3 lg:w-1/4 px-4 mb-8 lg:mb-0">
                 <h2 className="mb-2 text-4xl md:text-5xl text-coolGray-900 font-bold tracking-tighter">
-                  $30.000
+                  $ {totalIncome}
                 </h2>
                 <p className="text-lg md:text-xl text-green-500 font-medium">
                   Income
@@ -69,7 +101,7 @@ const AccountDetails = () => {
               </div>
               <div className="w-full md:w-1/3 lg:w-1/4 px-4">
                 <h2 className="mb-2 text-4xl md:text-5xl text-coolGray-900 font-bold tracking-tighter">
-                  $ 500
+                  $ {totalBalance}
                 </h2>
                 <p className="text-lg md:text-xl text-blue-500 font-medium">
                   Balance
